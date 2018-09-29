95458bb25c7ea53ad9ea02f34ec5489dd5bb68cb
Abdur Rafey <rafey7@abdurs-mbp.home>
2018-09-29 14:37:39 -0400

    Initial Commit

---
+++ FlixLab1UITests/FlixLab1UITests.swift
@@ -0,0 +1,36 @@
+//
+//  FlixLab1UITests.swift
+//  FlixLab1UITests
+//
+//  Created by Abdur Rafey on 9/29/18.
+//  Copyright © 2018 Abdur Rafey. All rights reserved.
+//
+
+import XCTest
+
+class FlixLab1UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
