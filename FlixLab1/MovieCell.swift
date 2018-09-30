6626a23f05f082251e8efc868188838075855651
Abdur Rafey <rafey7@Abdurs-MBP.home>
2018-09-30 15:55:20 -0400

    no loading icon

---
+++ FlixLab1/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  FlixLab1
+//
+//  Created by Abdur Rafey on 9/29/18.
+//  Copyright © 2018 Abdur Rafey. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
