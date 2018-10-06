a4c20a372d546e10008039ebef68ce4f7297779c
Abdur Rafey <rafey7@Abdurs-MBP.home>
2018-10-06 16:46:28 -0400

    Finished Required Stories

---
+++ FlixLab1/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  FlixLab1
+//
+//  Created by Abdur Rafey on 10/6/18.
+//  Copyright © 2018 Abdur Rafey. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
